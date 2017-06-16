ea210f753520a026eceafeeebf08cd20dec3b387
= <=>
2017-06-16 21:47:43 +0530

    Modified

--- Prime.java
+++ Prime.java
@@ -1,11 +1,11 @@
 class Prime{  
  		public static void main(String args[])	
 		{  
-  			int i,m=0,flag=0;    
+  			int i,num=0,flag=0;    
   			int n=2;//I've changed the num that is to be checked  
-  			m=n/2; 
+  			num=n/2; 
    
-  			for(i=2;i<=m;i++)
+  			for(i=2;i<=num;i++)
 			{    
    				if(n%i==0)
 				{    
@@ -20,3 +20,4 @@ class Prime{
 }  
 
 //Indented the file
+//changed m to num
